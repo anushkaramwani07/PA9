1880d4534959d8e2d4f8e70e480e624fb1ec82c9
AbiKubli <unicorns8abigail@gmail.com>
2025-04-13 15:27:33 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "GameBoard.hpp"
 
+// sfml-system-d.lib
+// sfml-window-d.lib
+// sfml-graphics-d.lib
+// sfml-audio-d.lib
+// sfml-network-d.lib
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 270, 270 }), "Lets Play!");
