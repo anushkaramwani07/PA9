632502d7dab0eb417cbd5ffdac89fb9c3b556d89
AbiKubli <unicorns8abigail@gmail.com>
2025-04-13 16:03:43 -0700

    Update GameBoard.cpp

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -2,7 +2,7 @@
 
 void GameBoard::setGameBoard(sf::RenderWindow& window)
 {
-    sf::Vector2f size(30, 30);
+    sf::Vector2f size(29.5, 29.5);
     sf::Vector2f pos;
     
     int yPos = 0; //the row we are at
@@ -11,76 +11,27 @@ void GameBoard::setGameBoard(sf::RenderWindow& window)
     {
         int xPos = 0; // which column we are in
 
-        if (yPos == 0 || yPos == 2 || yPos == 4 || yPos == 6) // does this row start with a white square
+        while (xPos < 8)
         {
-            while (xPos < 4)
-            {
-                pos.x = xPos * 60; // x position
-                pos.y = yPos * 30; // y postition
+            pos.x = xPos * 30; // x position
+            pos.y = yPos * 30; // y postition
 
-                /*this->setSize(size);
-                this->setFillColor(sf::Color(23, 43,53));
-                this->setPosition(pos);*/
+           /* this->setSize(size);
+            this->setFillColor(sf::Color(23, 43, 53));
+            this->setPosition(pos);*/
 
 
-                this->setSize(size);
-                this->setFillColor(sf::Color::Black);
-                this->setOutlineThickness(0.3);
-                this->setOutlineColor(sf::Color::White);
-                this->setPosition(pos);
+            this->setSize(size);
+            this->setFillColor(sf::Color::Black);
+            this->setOutlineThickness(0.5);
+            this->setOutlineColor(sf::Color::White);
+            this->setPosition(pos);
 
-                window.draw(*this);
+            window.draw(*this);
 
-                ++xPos;
-            }
+            ++xPos;
         }
-        else if (yPos == 1 || yPos == 3 || yPos == 5 || yPos == 7) // does this row start with a black square
-        {
-            while (xPos < 4)
-            {
-                pos.x = xPos * 60 + 30; // x position
-                pos.y = yPos * 30; // y postition
-
-               /* this->setSize(size);
-                this->setFillColor(sf::Color(23, 43, 53));
-                this->setPosition(pos);*/
-
-                this->setSize(size);
-                this->setFillColor(sf::Color::Black);
-                this->setOutlineThickness(0.3);
-                this->setOutlineColor(sf::Color::White);
-                this->setPosition(pos);
-
-                window.draw(*this);
-
-                ++xPos;
-            }
-        }
-
+        
         ++yPos;
     }
-
-    ////bottom cyan row (make for the index (1, 2, 3, 4, 5, 6, 7, 8))
-    //size.x = 270;
-    //size.y = 30;
-    //pos.x = 0;
-    //pos.y = 240;
-
-    //this->setSize(size);
-    //this->setFillColor(sf::Color::Cyan);
-    //this->setPosition(pos);
-
-    //window.draw(*this);
-
-    ////side cyan row (make for the index (A, B, C, D, E, F, G, H))
-    //size.x = 30;
-    //size.y = 240;
-    //pos.x = 240;
-    //pos.y = 0;
-
-    //this->setSize(size);
-    //this->setFillColor(sf::Color::Cyan);
-    //this->setPosition(pos);
-
-    //window.draw(*this);
 }
