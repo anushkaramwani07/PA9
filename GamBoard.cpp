1ff043ea9d3cd55bd5a0fcfc1bb35c513fe4b693
AbiKubli <unicorns8abigail@gmail.com>
2025-04-13 10:17:06 -0700

    Update GamBoard.cpp

--- GamBoard.cpp
+++ GamBoard.cpp
@@ -1,41 +1,52 @@
 #include "GameBoard.hpp"
 
-void GameBoard::displayBoard(sf::RenderWindow& window)
+void GameBoard::setGameBoard(sf::RenderWindow& window)
 {
-    GameBoard board(sf::Vector2f(window.getSize().x, window.getSize().y),
-        sf::Vector2f(0, 0), sf::Color::White); //creating a white backround
-    //now i will create the checkered effect
+    sf::Vector2f size(30, 30);
+    sf::Vector2f pos;
+    
+    int yPos = 0;
 
-    //window.getSize().x / 8, window.getSize().y / 8
+    while (yPos < 8)
+    {
+        int xPos = 0;
 
-    //row 1
-    //GameBoard b1(sf::Vector2f(window.getSize().x / 8, window.getSize().y / 8), sf::Vector2f(0, 0), sf::Color::Black);
-    GameBoard b1(sf::Vector2f(30, 30), sf::Vector2f(0, 0), sf::Color::Black);
-    GameBoard b2(sf::Vector2f(30, 30), sf::Vector2f(60, 0), sf::Color::Black);
-    GameBoard b3(sf::Vector2f(30, 30), sf::Vector2f(120, 0), sf::Color::Black);
-    GameBoard b4(sf::Vector2f(30, 30), sf::Vector2f(180, 0), sf::Color::Black);
+        if (yPos == 0 || yPos == 2 || yPos == 4 || yPos == 6)
+        {
+            while (xPos < 4)
+            {
+                //pos = { xPos * 60, yPos * 30 };
+                pos.x = xPos * 60;
+                pos.y = yPos * 30;
 
-    //row 2
-    //GameBoard b1(sf::Vector2f(window.getSize().x / 8, window.getSize().y / 8), sf::Vector2f(0, 0), sf::Color::Black);
-    GameBoard b5(sf::Vector2f(30, 30), sf::Vector2f(30, 30), sf::Color::Black);
-    GameBoard b6(sf::Vector2f(30, 30), sf::Vector2f(90, 30), sf::Color::Black);
-    GameBoard b7(sf::Vector2f(30, 30), sf::Vector2f(150, 30), sf::Color::Black);
-    GameBoard b8(sf::Vector2f(30, 30), sf::Vector2f(210, 30), sf::Color::Black);
-    //row 3
-    //row 4
-    //row 5
-    //row 6
-    //row 7
-    //row 8
+                this->setSize(size);
+                this->setFillColor(sf::Color::White);
+                this->setPosition(pos);
 
+                window.draw(*this);
 
-    window.draw(board);
-    window.draw(b1);
-    window.draw(b2);
-    window.draw(b3);
-    window.draw(b4);
-    window.draw(b5);
-    window.draw(b6);
-    window.draw(b7);
-    window.draw(b8);
+                ++xPos;
+            }
+        }
+        else if (yPos == 1 || yPos == 3 || yPos == 5 || yPos == 7)
+        {
+            while (xPos < 4)
+            {
+                //pos = { xPos * 60 + 30, yPos * 30 };
+
+                pos.x = xPos * 60 + 30;
+                pos.y = yPos * 30;
+
+                this->setSize(size);
+                this->setFillColor(sf::Color::White);
+                this->setPosition(pos);
+
+                window.draw(*this);
+
+                ++xPos;
+            }
+        }
+
+        ++yPos;
+    }
 }
