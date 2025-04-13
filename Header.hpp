e264e5142c9346b05fe713512fce2e8b4e693b78
AbiKubli <unicorns8abigail@gmail.com>
2025-04-13 10:29:18 -0700

    Create Header.hpp

---
+++ Header.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using sf::RectangleShape;
+using sf::Vector2f;
