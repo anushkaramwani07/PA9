9b383f8a23f355895e908af56a54cc1d5c11bb99
AbiKubli <unicorns8abigail@gmail.com>
2025-04-14 15:08:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Lets Play The Maze Game!");
     Player main(20.f, Vector2f(30, 30), sf::Color(236, 255, 173));
-    Maze Board(Vector2f(0, 0), Vector2f(0, 0), sf::Color::White);
+    Maze board(Vector2f(0, 0), Vector2f(0, 0), sf::Color::White);
     sf::Font font("Game Bubble.ttf");
 
-    int gameDiff = 0, moveX = 0, moveY = 0;
+    int gameDiff = 0, moveX = 1, moveY = 1;
 
     // easy, medium, hard
     // we want "enimies" that they can't always see and when they jump into the spot they lose
@@ -27,13 +27,23 @@ int main()
         window.clear();
         if (gameDiff == 0) //the game difficulty hasn't been selected
         {
-            
+            board.gameMenu(window, font);
 
 
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num1))
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num1)) // easy
             {
-                Board.easyMaze(window);
+                board.easyMaze(window);
                 gameDiff = 1;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num2)) // medium
+            {
+                gameDiff = 2;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num3)) // hard
+            {
+                gameDiff = 3;
             }
         }
         else // change screen because game difficulty has been selected
@@ -41,11 +51,23 @@ int main()
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
             {
                 //.move({ 0, 0.03 });
-                main.setYPos(main.getYPos() + 100);
-                ++moveY;
+                if (moveY < 8)
+                {
+                    ++moveY;
+
+                    if (board.checkSpot(main, moveX, moveY) == true)
+                    {
+                        main.setYPos(main.getYPos() + 100);
+                    }
+                    else
+                    {
+                        --moveY;
+                    }
+                    
+                }
             }
 
-            Board.setMaze(window, font, main);
+            board.setMaze(window, font, main);
         }
         window.display();
     }
