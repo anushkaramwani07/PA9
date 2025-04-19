4576b9a9d45ee7658080ee80dd16bb7848aab154
AbiKubli <unicorns8abigail@gmail.com>
2025-04-19 14:38:44 -0700

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Programmers: Abaigail Kubli and Anushka Ramwani
+ * Class: CptS 122; Lab Section 10
+ * Programming Assignment: 9 - A Graphical Game or Application
+ * Date: 4/12/2025
+ *
+ * Description: Creating our own game using sfml and inheritance
+ *
+ ******************************************************************************/
+
 #pragma once
 
 #include "Header.hpp"
@@ -5,8 +15,17 @@
 class Player : public sf::CircleShape
 {
 public:
-	//constructor
-	Player(const float& rad, const Vector2f& pos, sf::Texture mTexture, sf::RenderWindow& window) : mSprite(mTexture), CircleShape(rad)
+	/*************************************************************
+	* Function: Player()
+	* Date Created: 4/14/25
+	* Date Last Modified: 4/18/25
+	* Description: constructor for the Player
+	* Input parameters: const float& rad, const Vector2f& pos
+	* Returns: nothing
+	* Preconditions: none
+	* Postconditions: none
+	*************************************************************/
+	Player(const float& rad, const Vector2f& pos, sf::Texture texture) : mSprite(texture), CircleShape(rad)
 	{
 		mColor = sf::Color(236, 255, 173);
 		mSize = rad;
@@ -15,19 +34,23 @@ public:
 		this->setFillColor(sf::Color(236, 255, 173));
 		this->setPosition(pos);
 		this->setPosition(pos);
-
-		/*mTexture.loadFromFile("spider.png");
-		mSprite.setTexture(mTexture);
-		mSprite.setScale({4.f, 1.6f});*/
 	}
 
-	////destructor
-	//~Player();
 
+	/*************************************************************
+	* Function: Player()
+	* Date Created: 4/14/25
+	* Date Last Modified: 4/18/25
+	* Description: constructor for the Player
+	* Input parameters: const float& rad, const Vector2f& pos
+	* Returns: nothing
+	* Preconditions: none
+	* Postconditions: none
+	*************************************************************/
 	//setters
 	void setXPos(double newXPos);
 	void setYPos(double newYPos);
-	void setmSprite(sf::RenderWindow& window);
+	void setSprite(sf::RenderWindow& window, sf::Texture texture);
 
 	//getters
 	double getXPos();
@@ -38,6 +61,5 @@ private:
 	float mSize;
 	double xPos;
 	double yPos;
-	sf::Texture mTexture;
 	sf::Sprite mSprite;
 };
