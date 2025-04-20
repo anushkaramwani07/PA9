cc541ef613801debfad8881c9ee1f0e4722c29f8
AbiKubli <unicorns8abigail@gmail.com>
2025-04-20 13:14:56 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Programmers: Abaigail Kubli and Anushka Ramwani
+ * Class: CptS 122; Lab Section 10
+ * Programming Assignment: 9 - A Graphical Game or Application
+ * Date: 4/12/2025
+ *
+ * Description: Creating our own game using sfml and inheritance
+ *
+ ******************************************************************************/
+
 #include "test.hpp"
 
 /*************************************************************
@@ -47,9 +57,9 @@ void Test::testSetSprite()
     sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Lets Play The Maze Game!");
     sf::Texture texture("spider.png", false, sf::IntRect({ 0,0 }, { 638,532 }));
     sf::Sprite spider(texture);
-    Player m(40.f, Vector2f(400, 400), texture);
+    Player p(40.f, Vector2f(400, 400), texture);
 
-    cout << "X Position: " << m.getXPos() << " Y Position: " << m.getYPos(); // we will see the postion the spider is set too
+    cout << "X Position: " << p.getXPos() << " Y Position: " << p.getYPos(); // we will see the postion the spider is set too
 
     while (window.isOpen())
     {
@@ -60,7 +70,83 @@ void Test::testSetSprite()
         }
 
         window.clear();
-        m.setSprite(window, texture); // this should now place the top left corner of the spider in the middle of the screen
+        p.setSprite(window, texture); // this should now place the top left corner of the spider in the middle of the screen
         window.display();
     }
+}
+
+/*************************************************************
+    * Function: testCheckSpot()
+* Date Created: 4/20/25
+* Date Last Modified: 4/20/25
+* Description: tests checkSpot() and sees if it can actually
+    check each part of the array to see if the player can move there
+* Input parameters: none
+* Returns: nothing
+* Preconditions: none
+* Postconditions: none
+*************************************************************/
+void Test::testCheckSpot()
+{
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Lets Play The Maze Game!");
+    sf::Texture texture("spider.png", false, sf::IntRect({ 0,0 }, { 638,532 }));
+    sf::Sprite spider(texture);
+    sf::Font font("Game Bubble.ttf");
+    Player p(20.f, Vector2f(30, 30), texture);
+    Maze m(Vector2f(0, 0), Vector2f(0, 0), sf::Color::Transparent);
+
+    m.easyMaze();
+    m.printBoard(); // should display what the board now looks like
+
+    //true means that they can travel to that spot
+    if (m.checkSpot(p, 10, -1) == false) //out of bounds
+    {
+        cout << "Checking out of bounds: False" << endl;
+        
+        if (m.checkSpot(p, 8, 8) == true) // this is the end of the maze in the array
+        {
+            cout << "Checking end of maze spot: True" << endl;
+
+            if (m.checkSpot(p, 1, 1) == true) // start of the easy maze
+            {
+                cout << "Checking start of maze spot: True" << endl;
+
+                if (m.checkSpot(p, 1, 8) == false) // spot that is blocked in the maze
+                {
+                    cout << "Checking blocked part of maze spot: False" << endl;
+
+                    if (m.checkSpot(p, 1, 8) == false) // check the spot again now that checkSpot() has changed that array entry
+                    {
+                        cout << "Checking blocked part of maze spot: False" << endl;
+
+                        if (m.checkSpot(p, 1, 5) == true) // part of maze that is a path
+                        {
+                            cout << "Checking path part of maze spot: True" << endl;
+                        }
+                        else {
+                            cout << "Checking path part of maze spot: False (Wrong)" << endl;
+                        }
+                    }
+                    else {
+                        cout << "Checking blocked part of maze spot: True (Wrong)" << endl;
+                    }
+                }
+                else {
+                    cout << "Checking blocked part of maze spot: True (Wrong)" << endl;
+                }
+            }
+            else {
+                cout << "Checking start of maze spot: Flase (Wrong)" << endl;
+            }
+        }
+        else {
+            cout << "Checking end of maze spot: Flase (Wrong)" << endl;
+        }
+    }
+    else {
+        cout << "Checking out of bounds: True (Wrong)" << endl;
+    }
+
+    m.printBoard(); // has the board changed?
+    //does the postion [7][0] in the array now say -1
 }
