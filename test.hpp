a8d30dfdea3d4b851d3f3784366bf2c4c635cb3d
AbiKubli <unicorns8abigail@gmail.com>
2025-04-20 12:40:08 -0700

    Update test.hpp

--- test.hpp
+++ test.hpp
@@ -1,1 +1,43 @@
+#pragma once
+
+#include "Maze.hpp"
+
+class Test
+{
+public:
+	/*************************************************************
+	* Function: testMazeConstructor()
+	* Date Created: 4/19/25
+	* Date Last Modified: 4/19/25
+	* Description: tests the constructor for the maze
+	* Input parameters: none
+	* Returns: nothing
+	* Preconditions: none
+	* Postconditions: none
+	*************************************************************/
+	void testMazeConstructor();
+
+	/*************************************************************
+	* Function: testSetSprite()
+	* Date Created: 4/20/25
+	* Date Last Modified: 4/20/25
+	* Description: tests the setting the sprite image for the player
+	* Input parameters: none
+	* Returns: nothing
+	* Preconditions: none
+	* Postconditions: none
+	*************************************************************/
+	void testSetSprite();
+
+	/*************************************************************
+	* Function: testMazeConstructor()
+	* Date Created: 4/19/25
+	* Date Last Modified: 4/19/25
+	* Description: tests the constructor for the maze
+	* Input parameters: none
+	* Returns: nothing
+	* Preconditions: none
+	* Postconditions: none
+	*************************************************************/
+};
 
