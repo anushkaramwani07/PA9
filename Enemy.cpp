0c08c8d212819f5007c0cd1c7a054c85b99bf8d4
AbiKubli <unicorns8abigail@gmail.com>
2025-04-21 11:43:31 -0700

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,18 +1,30 @@
+/*******************************************************************************
+ * Programmers: Abaigail Kubli and Anushka Ramwani
+ * Class: CptS 122; Lab Section 10
+ * Programming Assignment: 9 - A Graphical Game or Application
+ * Date: 4/12/2025
+ *
+ * Description: Creating our own game using sfml and inheritance
+ *
+ ******************************************************************************/
+
 #include "Enemy.hpp"
 
+/*************************************************************
+* Function: rotateSprite()
+* Date Created: 4/21/25
+* Date Last Modified: 4/21/25
+* Description: rotates the enemy
+* Input parameters: string direction
+* Returns: nothing
+* Preconditions: none
+* Postconditions: none
+*************************************************************/
 void Enemy::rotateSprite(string direction)
 {
 	if (direction == "right")
 	{
-		mSprite.setScale(Vector2f(-1, -1));
-	}
-	else if (direction == "down")
-	{
-
-	}
-	else if (direction == "up")
-	{
-
+		mSprite.setScale(sf::Vector2f(-1, 1));
 	}
 	else if (direction == "left")
 	{
@@ -20,6 +32,16 @@ void Enemy::rotateSprite(string direction)
 	}
 }
 
+/*************************************************************
+* Function: checkIfPlayer()
+* Date Created: 4/21/25
+* Date Last Modified: 4/21/25
+* Description: sees if player and enemy are in the same place
+* Input parameters: Player p
+* Returns: true or false
+* Preconditions: none
+* Postconditions: none
+*************************************************************/
 bool Enemy::checkIfPlayer(Player p)
 {
 	if (p.getXPos() == this->getXPos())
@@ -32,6 +54,16 @@ bool Enemy::checkIfPlayer(Player p)
 	return false;
 }
 
+/*************************************************************
+* Function: moveEnemy()
+* Date Created: 4/21/25
+* Date Last Modified: 4/21/25
+* Description: moves the enemy on the board
+* Input parameters: Player p
+* Returns: nothing
+* Preconditions: none
+* Postconditions: none
+*************************************************************/
 void Enemy::moveEnemy(Player p)
 {
 	if (p.getXPos() > this->getXPos())
@@ -47,11 +79,38 @@ void Enemy::moveEnemy(Player p)
 	else if (p.getYPos() > this->getYPos())
 	{
 		this->setYPos(this->getYPos() + 100);
-		this->rotateSprite("up");
 	}
 	else if (p.getYPos() < this->getYPos())
 	{
 		this->setYPos(this->getYPos() - 100);
-		this->rotateSprite("down");
 	}
+}
+
+/*************************************************************
+* Function: setDamage()
+* Date Created: 4/21/25
+* Date Last Modified: 4/21/25
+* Description: setter for mDamage
+* Input parameters: int newDamage
+* Returns: nothing
+* Preconditions: none
+* Postconditions: none
+*************************************************************/
+void Enemy::setDamage(int newDamage)
+{
+	mDamage = newDamage;
+}
+/*************************************************************
+* Function: getDamage()
+* Date Created: 4/21/25
+* Date Last Modified: 4/21/25
+* Description: gets the mDamage
+* Input parameters: none
+* Returns: mDamage
+* Preconditions: none
+* Postconditions: none
+*************************************************************/
+int Enemy::getDamage()
+{
+	return this->mDamage;
 }
