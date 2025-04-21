91413664b478a9d218b7b684b3f64c37d5db9473
AbiKubli <unicorns8abigail@gmail.com>
2025-04-21 11:10:27 -0700

    Update Enemy.hpp

--- Enemy.hpp
+++ Enemy.hpp
@@ -4,10 +4,21 @@
 class Enemy : public Player
 {
 public:
-	Enemy(const float& rad, const Vector2f& pos, sf::Texture texture) : Player(rad, pos, texture)
+	Enemy(const float& rad, const Vector2f& pos, sf::Texture texture) : Player(rad, pos, texture) 
 	{
-
+		mDamage = 0;
 	};
 
 	//void kill(Player& player); // to attack
+
+	void rotateSprite(string direction) override;
+
+	bool checkIfPlayer(Player p);
+
+	void moveEnemy(Player p);
+
+
+
+private:
+	int mDamage;
 };
